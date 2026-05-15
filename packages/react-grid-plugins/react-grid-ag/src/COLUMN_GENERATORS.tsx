43967152908ee2028faa73f44da33a361aa27e10
amanx5 <110730645+amanx5@users.noreply.github.com>
2026-05-15 17:05:48 +0530

    feat(react-grid-ag): add AG Grid plugin package

---
+++ packages/react-grid-plugins/react-grid-ag/src/COLUMN_GENERATORS.tsx
@@ -0,0 +1,82 @@
+import type { PluginConfigAg } from "#types.ts";
+
+import {
+  sharedBooleanColumnGenerator,
+  sharedNumberColumnGenerator,
+  sharedStringColumnGenerator,
+  sharedStringDateColumnGenerator,
+  sharedUjsonObjectArrayColumnGenerator,
+  sharedUjsonObjectColumnGenerator,
+  sharedUjsonValueColumnGenerator,
+} from "@jsoc/grid-ag-shared";
+import {
+  COLUMN_DATA_TYPES,
+  type ColumnDataType,
+  type ColumnGenerator,
+  type ColumnGeneratorByType,
+  type GridRow,
+} from "@jsoc/grid-core";
+import { ChildGridToggle } from "@jsoc/react-grid";
+import type { ICellRendererParams } from "ag-grid-community";
+
+export type ColumnGeneratorAg<D extends ColumnDataType> = ColumnGenerator<
+  PluginConfigAg,
+  D
+>;
+
+const stringColumnGenerator: ColumnGeneratorAg<"string"> = (params) => {
+  return sharedStringColumnGenerator(params);
+};
+
+const booleanColumnGenerator: ColumnGeneratorAg<"boolean"> = (params) => {
+  return sharedBooleanColumnGenerator(params);
+};
+
+const numberColumnGenerator: ColumnGeneratorAg<"number"> = (params) => {
+  return sharedNumberColumnGenerator(params);
+};
+
+const stringDateColumnGenerator: ColumnGeneratorAg<"stringDate"> = (params) => {
+  return sharedStringDateColumnGenerator(params);
+};
+
+const ujsonObjectColumnGenerator: ColumnGeneratorAg<"ujsonObject"> = (
+  params,
+) => {
+  return {
+    ...sharedUjsonObjectColumnGenerator(params),
+    // https://www.ag-grid.com/react-data-grid/component-cell-renderer/
+    cellRenderer: (rParams: ICellRendererParams<GridRow, string>) => {
+      const { data } = rParams;
+      if (!data) return null;
+      return <ChildGridToggle row={data} columnKey={params.columnKey} />;
+    },
+  };
+};
+
+const ujsonObjectArrayColumnGenerator: ColumnGeneratorAg<"ujsonObjectArray"> = (
+  params,
+) => {
+  return {
+    ...sharedUjsonObjectArrayColumnGenerator(params),
+    cellRenderer: (rParams: ICellRendererParams<GridRow, string>) => {
+      const { data } = rParams;
+      if (!data) return null;
+      return <ChildGridToggle row={data} columnKey={params.columnKey} />;
+    },
+  };
+};
+
+const ujsonValueColumnGenerator: ColumnGeneratorAg<"ujsonValue"> = (params) => {
+  return sharedUjsonValueColumnGenerator(params);
+};
+
+export const COLUMN_GENERATORS: ColumnGeneratorByType<PluginConfigAg> = {
+  [COLUMN_DATA_TYPES.boolean]: booleanColumnGenerator,
+  [COLUMN_DATA_TYPES.number]: numberColumnGenerator,
+  [COLUMN_DATA_TYPES.string]: stringColumnGenerator,
+  [COLUMN_DATA_TYPES.stringDate]: stringDateColumnGenerator,
+  [COLUMN_DATA_TYPES.ujsonObject]: ujsonObjectColumnGenerator,
+  [COLUMN_DATA_TYPES.ujsonObjectArray]: ujsonObjectArrayColumnGenerator,
+  [COLUMN_DATA_TYPES.ujsonValue]: ujsonValueColumnGenerator,
+};
