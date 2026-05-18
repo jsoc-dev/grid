7fd8fb7b0aeae6e85b8b0a0a8cd97ad9149cab8b
amanx5 <110730645+amanx5@users.noreply.github.com>
2026-05-18 12:31:35 +0530

    chore: remove smoke tests

--- smoke-test/tests/grid-core.test.ts
+++
@@ -1,7 +0,0 @@
-import * as core from "@jsoc/grid-core";
-import { expect, test } from "vitest";
-
-test("@jsoc/grid-core loads correctly", () => {
-  expect(core).toBeDefined();
-  expect(Object.keys(core).length).toBeGreaterThan(0);
-});
--- smoke-test/tests/utils.test.ts
+++
@@ -1,7 +0,0 @@
-import * as utils from "@jsoc/utils";
-import { expect, test } from "vitest";
-
-test("@jsoc/utils loads correctly", () => {
-  expect(utils).toBeDefined();
-  expect(Object.keys(utils).length).toBeGreaterThan(0);
-});
