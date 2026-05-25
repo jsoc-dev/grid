da1d2c8bbe96a78685400093a507fbf2bdb34fa7
amanx5 <110730645+amanx5@users.noreply.github.com>
2026-05-26 04:21:10 +0530

    feat(utils): add ensureError helper

--- packages/utils/src/error.ts
+++ packages/utils/src/error.ts
@@ -1,3 +1,9 @@
 export function isError(arg: unknown): arg is Error {
   return arg instanceof Error;
 }
+
+export function ensureError(arg: unknown, msg?: string): Error {
+  return isError(arg)
+    ? arg
+    : new Error(msg ?? "An unknown error occurred.", { cause: arg });
+}
