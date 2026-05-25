bb3df4aeaa20071b54be693dbe5172d74c615c0e
amanx5 <110730645+amanx5@users.noreply.github.com>
2026-05-26 04:53:22 +0530

    chore(scripts): add support for building all packages

--- scripts/build-packages.ts
+++ scripts/build-packages.ts
@@ -1,13 +1,20 @@
 import { REPO_DIR } from "#/repo.ts";
+import { findPackageDirectories } from "#/scripts/utils/findPackageDirectories.ts";
 import { getPackageJson } from "#/scripts/utils/getPackageJson.ts";
 import { logMilestone } from "#/scripts/utils/logMilestone.ts";
 
 import { resolve } from "node:path";
 import { pathToFileURL } from "node:url";
 
+import { ensureError, isPlainObject } from "@jsoc/utils";
 import { build, type UserConfig } from "tsdown";
 
 type BuildConfig = UserConfig;
+
+const CWD = process.cwd();
+const INIT_CWD = process.env.INIT_CWD;
+const PACKAGES_DIR = resolve(REPO_DIR, "packages");
+const CONFIG_FILE = "tsdown.config.ts";
 
 const DEFAULT_BUILD_CONFIG = {
   entry: { index: "src/index.ts" },
@@ -20,63 +27,83 @@ const DEFAULT_BUILD_CONFIG = {
   },
 } satisfies BuildConfig;
 
-const CWD = process.cwd();
-const INIT_CWD = process.env.INIT_CWD;
-
 await main();
 
 async function main() {
-  const packageDir = [INIT_CWD, CWD].find((dir) => dir && dir !== REPO_DIR);
+  const packageDirectory = [INIT_CWD, CWD].find(
+    (dir) => dir && dir !== REPO_DIR,
+  );
+  const packageDirectories = packageDirectory
+    ? [packageDirectory]
+    : await findPackageDirectories(PACKAGES_DIR);
 
-  if (packageDir) {
-    await buildPackage(packageDir);
-    return;
+  console.info(`📦 Number of packages to build: ${packageDirectories.length}`);
+  for (const dir of packageDirectories) {
+    try {
+      await buildPackage(dir);
+    } catch (error) {
+      console.error("❌ Failed to build package.", error);
+      process.exit(1);
+    }
   }
-
-  // TODO: Build all packages
-  console.log("Building all packages is currently not supported.");
+  console.info(`✅ Number of packages built: ${packageDirectories.length}`);
 }
 
-async function buildPackage(packageDir: string) {
-  try {
-    const packageJson = getPackageJson(packageDir);
+async function buildPackage(packageDirectory: string) {
+  const packageJson = getPackageJson(packageDirectory);
 
-    if (!packageJson) {
-      throw new Error(`No package.json found in ${packageDir}`);
+  if (!packageJson) {
+    throw new Error(`No package.json found in ${packageDirectory}`);
+  }
+
+  logMilestone(`📦 Build started for "${packageJson.name}"`, "start");
+  console.info(`Package directory: ${packageDirectory}`);
+  const config = await getBuildConfig(packageDirectory);
+
+  const previousCwd = process.cwd();
+
+  try {
+    process.chdir(packageDirectory);
+    await build(config);
+  } finally {
+    process.chdir(previousCwd);
+  }
+
+  logMilestone(`✅ Build completed for "${packageJson.name}"`, "end");
+}
+
+async function getBuildConfig(packageDirectory: string): Promise<BuildConfig> {
+  let overrides;
+
+  try {
+    const configPath = resolve(packageDirectory, CONFIG_FILE);
+    const configModuleUrl = pathToFileURL(configPath).href;
+    const configModule = (await import(configModuleUrl)) as unknown;
+
+    if (
+      !configModule ||
+      !isPlainObject(configModule) ||
+      !("default" in configModule) ||
+      !isPlainObject(configModule.default)
+    ) {
+      throw new Error(`${CONFIG_FILE} must default export an object.`);
     }
 
-    logMilestone(`📦 Build started for "${packageJson.name}"`, "start");
-    console.log("\nPackage directory:", packageDir);
-    const config = await getBuildConfig(packageDir);
-    process.chdir(packageDir);
-    await build(config);
-    process.chdir(CWD);
-    logMilestone(`✅ Build completed for "${packageJson.name}"`, "end");
-  } catch (error) {
-    console.error(`❌ Failed to build package. ${error}`);
-    process.exit(1);
+    overrides = configModule.default;
+  } catch (catched) {
+    const error = ensureError(catched);
+
+    if ("code" in error && error.code === "ERR_MODULE_NOT_FOUND") {
+      console.info(`\nNo ${CONFIG_FILE} found in the package directory.`);
+    } else {
+      console.warn(`\nFailed to find ${CONFIG_FILE}: `, error.message);
+    }
   }
-}
 
-async function getBuildConfig(packageDir: string): Promise<BuildConfig> {
-  const CONFIG_FILENAME = "tsdown.config.ts";
-
-  try {
-    const configPath = resolve(packageDir, CONFIG_FILENAME);
-    const configModuleUrl = pathToFileURL(configPath).href;
-    const configModule = await import(configModuleUrl);
-    const overrides = configModule.default;
-    console.info(`\nFound ${CONFIG_FILENAME} at ${configPath}.\n`);
-
-    return {
-      ...DEFAULT_BUILD_CONFIG,
-      ...overrides,
-    };
-  } catch {
-    console.info(
-      `\nUnable to find ${CONFIG_FILENAME}. Default build config will be used.\n`,
-    );
-
+  if (!overrides) {
+    console.info(`\nUsing default build config.\n`);
     return DEFAULT_BUILD_CONFIG;
   }
+
+  return { ...DEFAULT_BUILD_CONFIG, ...overrides };
 }
