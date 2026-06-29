47db18c599e8ab14f67354751b74242d7587f520
amanx5 <110730645+amanx5@users.noreply.github.com>
2026-06-29 22:53:55 +0530

    feat(grid-docs): add `getExampleAppGitHubUrl`

--- packages/grid-docs/src/utils/build-examples.ts
+++ packages/grid-docs/src/utils/build-examples.ts
@@ -4,6 +4,10 @@ import type { AdapterId, PluginId } from "#types.ts";
 export type ExamplesRelativePath<A extends AdapterId = AdapterId> =
   A extends AdapterId ? `examples/${A}/${PluginId<A>}` : never;
 
+/**
+ * Returns the relative path of the examples directory for the given adapter and plugin.
+ * This is a mutual contract between build-examples script and example app url getters to follow this url pattern.
+ */
 export function getExamplesRelativePath<A extends AdapterId>(
   adapterId: A,
   pluginId: PluginId<A>,
@@ -11,22 +15,40 @@ export function getExamplesRelativePath<A extends AdapterId>(
   return `examples/${adapterId}/${pluginId}` as ExamplesRelativePath<A>;
 }
 
-// Returns the url of a specific example app present in the public/ folder of docs app.
+/** Returns the github url of the example app for the given adapter and plugin. */
+export function getExampleAppGitHubUrl<
+  A extends AdapterId,
+  P extends PluginId<A>,
+>(adapterId: A, pluginId: P) {
+  const repo = "https://github.com/jsoc-dev/grid/tree/main";
+  const examplesPath = getExamplesRelativePath(adapterId, pluginId);
+  return `${repo}/${examplesPath}`;
+}
+
+/** Returns the url of a specific example html file present in the public/ folder of docs. */
+export function getExampleHtmlFileUrl<
+  A extends AdapterId,
+  P extends PluginId<A>,
+>(adapterId: A, pluginId: P) {
+  const examplesPath = getExamplesRelativePath(adapterId, pluginId);
+  return `/${examplesPath}/index.html`;
+}
+
+/** Returns the url of a specific example html file present in the public/ folder of docs app along with exampleId as search param. */
 export function getExampleUrl<A extends AdapterId, P extends PluginId<A>>(
   adapterId: A,
   pluginId: P,
   exampleId: ExampleId<A, P>,
 ) {
-  const examplesPath = getExamplesRelativePath(adapterId, pluginId);
-  const indexFilePath = `/${examplesPath}/index.html`;
+  const indexFilePath = getExampleHtmlFileUrl(adapterId, pluginId);
 
   return indexFilePath + buildExampleIdSearchQuery(exampleId);
 }
 
-// Name of the query parameter that will contain the example id.
+/** Name of the query parameter that will contain the example id. */
 export const EXAMPLE_ID_SEARCH_PARAM = "exampleId";
 
-// Builds a search query that can be used to find a example by exampleId
+/** Builds a search query that can be used to find a example by exampleId */
 export function buildExampleIdSearchQuery<E extends string>(exampleId: E) {
   return "?" + EXAMPLE_ID_SEARCH_PARAM + "=" + exampleId;
 }
