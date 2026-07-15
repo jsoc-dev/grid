a25ff7da15c45791d5097f4a6c45480f198ffb74
AManWhoCodes <110730645+amanx5@users.noreply.github.com>
2026-07-15 17:43:36 +0530

    refactor(docs): remove Root.tsx file
    - the contents of Root.tsx is moved to layout.tsx as keeping them separate is unnecessary
    
    - Footer is added back again, as it indirectly makes sure the vertical scrollbar is always there,
    
    - As a result there are no layout shifts while navigating between short and long pages.

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,9 +1,14 @@
 import "@/app/globals.css";
 import "nextra-theme-docs/style.css";
-import { Root } from "@/components/root/Root";
 
 import type { Metadata } from "next";
+import { Footer } from "@/components/root/Footer";
+import { Navbar } from "@/components/root/Navbar";
+import { QueryProvider } from "@/components/root/QueryProvider";
+import { ThemeProvider } from "@/components/root/ThemeProvider";
 import { Head } from "nextra/components";
+import { Layout } from "nextra-theme-docs";
+import { getPageMap } from "nextra/page-map";
 
 export const metadata: Metadata = {
   title: "JSOC Grid",
@@ -14,7 +19,20 @@ export default async function RootLayout({ children }: LayoutProps<"/">) {
     <html lang="en" dir="ltr" suppressHydrationWarning>
       <Head />
       <body>
-        <Root>{children}</Root>
+        <ThemeProvider>
+          <QueryProvider>
+            <Layout
+              darkMode={false} // hide theme toggle button as Navbar already has it
+              sidebar={{ defaultMenuCollapseLevel: 1 }}
+              navbar={<Navbar />}
+              pageMap={await getPageMap()}
+              docsRepositoryBase="https://github.com/jsoc-dev/grid/tree/main/docs"
+              footer={<Footer />}
+            >
+              {children}
+            </Layout>
+          </QueryProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
