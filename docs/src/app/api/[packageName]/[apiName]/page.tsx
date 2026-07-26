1edb278c7f5a544c37c5bdca9118078704aa1a04
AManWhoCodes <110730645+amanx5@users.noreply.github.com>
2026-07-26 20:31:49 +0530

    refactor(docs): replace MDX compilation with React components for API pages
    experimental change to optimise api page generation

--- docs/src/app/api/[packageName]/[apiName]/page.tsx
+++ docs/src/app/api/[packageName]/[apiName]/page.tsx
@@ -1,6 +1,6 @@
 import { getMDXComponents } from "@/mdx-components";
 import { getApiExports } from "@/utils/api/api-exports";
-import { generateApiPage } from "@/utils/api/generate-api-page";
+import { generateApiPage } from "@/components/api/api-page";
 import { generateApiDefinition } from "@/utils/api/generate-api-definition";
 import { API_PACKAGES, isValidApiPackageName } from "@/utils/api/api-packages";
 import { notFound } from "next/navigation";
@@ -37,12 +37,12 @@ export default async function Page(
   if (!apiExport) return notFound();
 
   const definition = await generateApiDefinition(apiExport);
-  const page = await generateApiPage(apiExport, definition);
-  const { default: MDXContent, toc, metadata, sourceCode } = page;
+  const { toc, content } = generateApiPage(apiExport, definition);
+  const metadata = { title: apiName, filePath: "" };
 
   return (
-    <Wrapper toc={toc} metadata={metadata} sourceCode={sourceCode}>
-      <MDXContent />
+    <Wrapper toc={toc} metadata={metadata} sourceCode={""}>
+      {content}
     </Wrapper>
   );
 }
---
+++ docs/src/components/api/api-page.tsx
@@ -0,0 +1,223 @@
+import { CodeBlock } from "@/components/CodeBlock";
+import { ApiDoc } from "@/components/api/api-doc";
+import { getMDXComponents } from "@/mdx-components";
+import {
+  checkDeclarationKind,
+  getModuleSpecifierRelativeToRoot,
+} from "@/utils/api/api-exports";
+import { withPackageScope } from "@/utils/api/api-packages";
+import type {
+  ApiExport,
+  GenerateDefinitionResult,
+} from "@/utils/api/api-reference-types";
+import {
+  withGithubMainBranchUrl,
+  withPackageGithubBaseUrl,
+} from "@jsoc/grid-docs";
+import type { Heading } from "nextra";
+import { Callout } from "nextra/components";
+import type { ReactNode } from "react";
+import type { ExportedDeclarations } from "ts-morph";
+
+export type GenerateApiPageResult = {
+  toc: Heading[];
+  content: ReactNode;
+};
+
+const {
+  a: Anchor,
+  code: Code,
+  details: Details,
+  h1: H1,
+  h2: H2,
+  p: Paragraph,
+  summary: Summary,
+} = getMDXComponents();
+
+/**
+ * (experimental) React components based API page generator introduced to replace
+ * the [raw mdx to page generator](../../utils/api/generate-api-page.ts).
+ */
+export function generateApiPage(
+  apiExport: ApiExport,
+  definition: GenerateDefinitionResult | undefined,
+): GenerateApiPageResult {
+  const { packageName, declaration } = apiExport;
+
+  if (!declaration) {
+    const sourceCodeUrl = withPackageGithubBaseUrl(packageName);
+
+    return {
+      toc: [],
+      content: (
+        <>
+          <Title apiExport={apiExport} />
+          <Paragraph>
+            We are still working on this page. Please check back later. In the
+            meantime, you can explore the{" "}
+            <Anchor href={sourceCodeUrl}>source code</Anchor> of its package.
+          </Paragraph>{" "}
+        </>
+      ),
+    };
+  }
+
+  const toc: Heading[] = [];
+  const sections: ReactNode[] = [];
+
+  const { isClass, isFunction, isType, isOther } =
+    checkDeclarationKind(declaration);
+
+  // Declaration (only for type aliases/interfaces)
+  if (isType) {
+    const declarationCode = (
+      <CodeBlock code={declaration.getText()} lang="typescript" />
+    );
+
+    const sectionKey = "declaration";
+    const sectionName = "Declaration";
+
+    if (definition) {
+      sections.push(
+        <Details key={sectionKey}>
+          <Summary>View {sectionName}</Summary>
+          {declarationCode}
+        </Details>,
+      );
+    } else {
+      toc.push({ depth: 2, value: sectionName, id: sectionKey });
+      sections.push(
+        <section key={sectionKey}>
+          <H2 id={sectionKey}>{sectionName}</H2>
+          {declarationCode}
+        </section>,
+      );
+    }
+  }
+
+  if (definition) {
+    // Specification
+    const specTitle = isClass
+      ? "Members"
+      : isFunction
+        ? "Signature"
+        : isType
+          ? "Fields"
+          : isOther && definition
+            ? "signatures" in definition
+              ? "Signature"
+              : "Properties"
+            : "Properties";
+    const specId = specTitle.toLowerCase();
+
+    toc.push({ depth: 2, value: specTitle, id: specId });
+    sections.push(
+      <section key="specification">
+        <H2 id={specId}>{specTitle}</H2>
+        <ApiDoc definition={definition} />
+      </section>,
+    );
+
+    // Throws
+    if (definition.tags?.throws) {
+      sections.push(
+        <Callout key="throws" type="warning">
+          {definition.tags.throws}
+        </Callout>,
+      );
+    }
+
+    // see also
+    if (definition.tags?.see) {
+      const sectionId = "see-also";
+      const sectionTitle = "See also";
+      toc.push({ depth: 2, value: sectionTitle, id: sectionId });
+      sections.push(
+        <section key={sectionId}>
+          <H2 id={sectionId}>{sectionTitle}</H2>
+          <Paragraph>{definition.tags.see}</Paragraph>
+        </section>,
+      );
+    }
+
+    // Example
+    if (definition.tags?.example) {
+      toc.push({ depth: 2, value: "Example", id: "example" });
+      sections.push(
+        <section key="example">
+          <H2 id="example">Example</H2>
+          <Paragraph>{definition.tags.example}</Paragraph>
+        </section>,
+      );
+    }
+
+    // Usage
+    if (definition.tags?.usage) {
+      toc.push({ depth: 2, value: "Usage", id: "usage" });
+      sections.push(
+        <section key="usage">
+          <H2 id="usage">Usage</H2>
+          <Paragraph>{definition.tags.usage}</Paragraph>
+        </section>,
+      );
+    }
+  }
+
+  return {
+    toc,
+    content: (
+      <>
+        <Title apiExport={apiExport} />
+        {definition?.description && (
+          <Paragraph>{definition.description}</Paragraph>
+        )}
+        <SourceCodeCallout apiExport={apiExport} declaration={declaration} />
+        {sections}
+      </>
+    ),
+  };
+}
+
+function Title({ apiExport }: { apiExport: ApiExport }) {
+  const { name: exportName, declaration } = apiExport;
+  const { isClass, isFunction, isType } = checkDeclarationKind(declaration);
+
+  const suffix = isClass
+    ? "class"
+    : isFunction
+      ? "function"
+      : isType
+        ? "type"
+        : ""; // can be a primitive or a re-export of a class/function/type.
+
+  return (
+    <H1>
+      <Code>{exportName}</Code>
+      {declaration && " " + suffix}
+    </H1>
+  );
+}
+
+function SourceCodeCallout({
+  apiExport,
+  declaration,
+}: {
+  apiExport: ApiExport;
+  declaration: ExportedDeclarations;
+}) {
+  const { packageName } = apiExport;
+  const sourceCodeUrl = withGithubMainBranchUrl(
+    getModuleSpecifierRelativeToRoot(declaration),
+  );
+
+  const packageNameWithScope = withPackageScope(packageName);
+  const packageLink = `https://www.npmjs.com/package/${packageNameWithScope}`;
+
+  return (
+    <Callout type="default">
+      This API reference is automatically generated from the{" "}
+      <Anchor href={sourceCodeUrl}>source code</Anchor> of the{" "}
+      <Anchor href={packageLink}>{packageNameWithScope}</Anchor> package.
+    </Callout>
+  );
+}
